370f8b9090aca4e15c7dada6a6c1d42792b6ecaf
seunghonice <seunghonice@gmail.com>
2022-03-20 20:09:05 +0900

    feat: QuizBrain 을 만들었다.

---
+++ lib/Question.dart
@@ -0,0 +1,6 @@
+class Question {
+  String questionText;
+  bool answer;
+
+  Question(this.questionText, this.answer);
+}
---
+++ lib/QuizBrain.dart
@@ -0,0 +1,48 @@
+import 'package:quizzler_flutter/Question.dart';
+
+class QuizBrain {
+  int questionNumber = 0;
+
+  List<Question> _questionBank = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+  ];
+
+  bool nextQuestion() {
+    if (questionNumber + 1< _questionBank.length) {
+      questionNumber++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String getQuestionText() => _questionBank[questionNumber].questionText;
+
+  bool getCorrectAnswer() => _questionBank[questionNumber].answer;
+}
